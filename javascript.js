dd55bd7ba733c233b9fb6917b650a2805584c870
Sheraz Ahsan <ahsan65dev@gamil.com>
2026-02-21 23:01:08 +0500

    video 14

---
+++ javascript.js
@@ -0,0 +1,15 @@
+
+let b = 10;
+let a
+console.log("a+b", a+b);
+console.log("a*b", a*b);
+console.log("b-a", b-a);
+console.log("b/a", b/a);
+console.log("a%b", a%b);
+console.log("a**b", a**b);
+console.log("++a", ++a);
+console.log("b--", b--);
+console.log("Final values: a =", a, ", b =", b);
+console.log("a==b", a==b);
+console.log("a!=b", a!=b);
+console.log("a>b", a>b);
